bf2f7bfd40407398b26d5de60c861e25974f7151
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 05:10:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,6 @@ def all_supplies_in_holidays(holiday_hash)
       puts "  " + temp + ": " + holiday_hash[season][holiday].join(", ")
     end
   end
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
