7b0e395e0e88591e5a742cde8fcaf07eb3c273c1
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 04:50:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,9 @@ def all_supplies_in_holidays(holiday_hash)
     puts season.to_s.capitalize + ":"
     holiday_hash[season].keys.each do |holiday|
       if holiday.include?("_")
+        temp = holiday.split("_")
+        temp.capitalize!
+        temp.join(" ")
     end
   end
 
