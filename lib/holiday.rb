b1958227ad509bbff583ad622b47e5de50becfb4
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 04:37:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,10 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
       #end
     #end
   #end
-  holiday_hash
+  holiday_hash[season].each do |season, holiday|
+    
+    
+  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
